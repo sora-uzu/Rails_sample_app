c5f0fc3ff5ad61e99ff97e365aad7a06ed16126b
sora-uzu <naruto07170717@gmail.com>
2020-02-16 15:54:20 +0900

    [Finish]3-10

---
+++ sample_app/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = 'e44091447b7667809fb9c1c29e910def6c2ae84eadde934f59f6330e50d4e155f51012f0b6a7102351c10d1e55da0b6b8cfe5d15b158bb55edd249475ced5e47'
